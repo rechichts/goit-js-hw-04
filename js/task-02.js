52f2ae6727cbad81111c3f9bf7040bae4a0a620a
TatyanaRS <rechich.ts@gmail.com>
2020-03-11 22:56:44 +0200

    'first'

---
+++ js/task-02.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const inventory = {
+  items: ['Knife', 'Gas mask'],
+  add(itemName) {
+    console.log(`Adding ${itemName} to inventory`);
+
+    this.items.push(itemName);
+  },
+  remove(itemName) {
+    console.log(`Removing ${itemName} from inventory`);
+
+    this.items = this.items.filter(item => item !== itemName);
+  },
+};
+
+const invokeInventoryOperation = function(itemName, inventoryAction) {
+  console.log(`Invoking ${inventoryAction.name} opeartion on ${itemName}`);
+  inventoryAction.call(inventory, itemName);
+};
+
+invokeInventoryOperation('Medkit', inventory.add);
+// Invoking action on Medkit
+// Adding Medkit to inventory
+
+console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
+invokeInventoryOperation("Gas mask", inventory.remove);
+// Invoking action on Gas mask
+// Removing Gas mask from inventory
+
+console.log(inventory.items); // ['Knife', 'Medkit']
